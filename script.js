f1797c253e962fbc118085a270ab4342b9af40fd
esportista <of.glauco@gmail.com>
2025-01-29 20:08:32 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Função para exibir/ocultar o menu
+function toggleMenu() {
+  const menu = document.getElementById('menu');
+  if (menu.style.display === 'block') {
+    menu.style.display = 'none';
+  } else {
+    menu.style.display = 'block';
+  }
+}
